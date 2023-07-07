7a9f07ccf9be3553c860cec32263818b911a173d
Tirth <tirthcoder@gmail.com>
2023-07-07 10:06:46 +0530

    Structure of app and model created

---
+++ src/controllers/city-controller.js
@@ -0,0 +1,83 @@
+const { CityService } = require("../services/index");
+
+const cityService = new CityService();
+
+const create = async (req, res) => {
+  try {
+    const city = await cityService.createCity(req.body);
+    return res.status(201).json({
+      data: city,
+      success: true,
+      message: "Successfully created a city",
+      err: {},
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      data: {},
+      success: false,
+      message: "Not able to create city",
+      err: error,
+    });
+  }
+};
+
+const get = async (req, res) => {
+  try {
+    const city = await cityService.getCity(req.params.id);
+    return res.status(200).json({
+      data: city,
+      success: true,
+      message: "Successfully fetched a city",
+      err: {},
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      data: {},
+      success: false,
+      message: "Not able to fetch city",
+      err: error,
+    });
+  }
+};
+
+const update = async (req, res) => {
+  try {
+    const city = await cityService.updateCity(req.params.id, req.body);
+    return res.status(200).json({
+      data: city,
+      success: true,
+      message: "Successfully updated a city",
+      err: {},
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      data: {},
+      success: false,
+      message: "Not able to update city",
+      err: error,
+    });
+  }
+};
+
+const destroy = async (req, res) => {
+  try {
+    const city = await cityService.destroyCity(req.params.id);
+    return res.status(200).json({
+      data: city,
+      success: true,
+      message: "Successfully deleted a city",
+      err: {},
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      data: {},
+      success: false,
+      message: "Not able to delete city",
+      err: error,
+    });
+  }
+};
